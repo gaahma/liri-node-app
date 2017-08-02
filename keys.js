e37af1a99d6c39215f0e3522c93a298a5a57cf5c
Adam Haag <adam.m.h@gmail.com>
2017-08-02 15:35:45 -0400

    liri

---
+++ keys.js
@@ -0,0 +1,15 @@
+exports.twitterKeys = {
+  consumer_key: 	"pDjfRX9qEEKD7k1Bcljs0swna",
+  consumer_secret: 'uJmDENGSbkv0Pp3vDZUAXEXFz2eVEHuIfysRDnnbaDlb8Mcmsn',
+  access_token_key: "890588549451579392-UznuxohvSIu5E8k3JkQG6SQciIlxkux",
+  access_token_secret: 'D6tAhaYEuAtGIhBX3T1n8kwBnxgcsGQfW0CV0lNxhZyD8',
+}
+
+exports.spotifyKeys = {
+    id: "faf3de6759da4f2d8203d268bd91fb2b",
+    secret: "efb664aeaf6a4b90a61dbb36ac9a2767" 
+}
+
+exports.omdbKey = {
+    apikey: "40e9cece"
+}
---
+++ liri.js
@@ -0,0 +1,143 @@
+var keys = require("./keys.js");
+var moment = require("moment");
+var command = process.argv[2];
+var userInput = "";
+
+
+for(var i = 3; i < process.argv.length; i++){
+    userInput += process.argv[i] + " ";
+} 
+
+
+liri();
+
+function liri(){
+    writeToLog();
+    switch (command){
+        case "spotify":
+            spotifyThisSong();
+            break;
+        case "twitter":
+            myTweets();
+            break;
+        case "movie":
+            movieThis();
+            break;
+        case "do-this":
+            doThis();
+            break;
+        default:
+            console.log(`Cannot run command: ${command}`);
+    }
+}
+
+
+
+
+function spotifyThisSong(){
+    if (userInput === "")
+        userInput = "The Sign"
+    console.log(`Searching for ${userInput}`);
+
+    var Spotify = require('node-spotify-api');
+    var spotify = new Spotify({
+        id: keys.spotifyKeys.id,
+        secret: keys.spotifyKeys.secret
+    });
+    
+    spotify.search({ type: 'track', query: userInput }, function(err, data) {
+        if (err) 
+            return console.log('Error occurred: ' + err);
+        var items = data.tracks.items;
+
+        if(items.length === 0){
+            console.log("No results found")
+        } else {
+            for (var i = 0; i < items.length; i++){
+                console.log("\nArtist Name: " + items[i].album.artists[0].name);
+                console.log("Album Name: " + items[i].album.name);
+                console.log("Album link: " + items[i].album.external_urls.spotify + "\n");
+            }
+        }
+    });
+}
+
+function myTweets(){
+    var Twitter = require('twitter');
+
+    console.log("Loading keys...");
+    var client = new Twitter({
+        consumer_key: keys.twitterKeys.consumer_key,
+        consumer_secret: keys.twitterKeys.consumer_secret,
+        access_token_key: keys.twitterKeys.access_token_key,
+        access_token_secret: keys.twitterKeys.access_token_secret
+    });
+
+    var params = {screen_name: 'gaahmamhaag'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for(var i = 0; i < tweets.length; i++){
+                var time = tweets[i].created_at.split("+")
+                console.log("\nTweet: " + tweets[i].text);
+                console.log("Tweeted: " + time[0].trim() + "\n");     
+            }
+        } else {
+            console.log(error);
+        }
+    });
+}
+
+function movieThis(){
+    var request = require("request");
+    if (userInput === "")
+        userInput = "Mr. Nobody";
+    var queryUrl = `http://www.omdbapi.com/?t=${userInput}&y=&plot=short&apikey=40e9cece`;
+    
+    request(queryUrl, function(error, response, body){
+        if(!error && response.statusCode === 200){
+            var bodyObj = JSON.parse(body, null, 2);
+            if(bodyObj.Response === "False"){
+                console.log("No results");
+                return;
+            }
+            //console.log(bodyObj);
+            var year = bodyObj.Released;
+            year = year.split(" ");
+            console.log(`\nTitle: ${bodyObj.Title}`)
+            console.log(`Year: ${year[2]}\nIMDB: ${bodyObj.imdbRating}`);
+            if(bodyObj.Ratings[1].Source === "Rotten Tomatoes"){
+                console.log(`${bodyObj.Ratings[1].Source}: ${bodyObj.Ratings[1].Value}`);
+            }
+            console.log(`Filmed in: ${bodyObj.Country}\nLanguage: ${bodyObj.Language}`);
+            console.log(`Plot: ${bodyObj.Plot}\nActors: ${bodyObj.Actors}`);
+        }
+    });
+}
+
+function doThis(){
+    var fs = require("fs");
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        if (err) 
+            return console.log(err);
+        //console.log(data);  
+        var liriDo = data.split(" ");
+        command = liriDo[0];
+        for (var i = 1; i < liriDo.length; i++){
+            userInput += liriDo[i] + " ";
+        }
+        liri();
+    });
+}
+
+function writeToLog(){
+    var fs = require("fs");
+    var time = moment().format();
+    fs.appendFile("log.txt", `${time} ${command} ${userInput}\n`, function (err){
+        if (err){
+            return console.log(err);
+        }
+        console.log("Activity logged");
+    });
+}
+
+
